83f7db0308c9e64548b3bb20b2b8e850ee9a5277
Deepak Patidar <deepak2025patidar@gmail.com>
2024-01-11 14:56:14 +0530

    Add new rotes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import {Router} from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controler.js";
+import { ChangeCurrentPasssword, 
+     getCurentUser, 
+     getUserChannelProfile,
+     getWatchHistory, 
+     loginUser, 
+     logoutUser,
+     refreshAccessToken,
+     registerUser, 
+     updateAccountDetails, 
+     updateCoverImage, 
+     updateUserAvatar } from "../controllers/user.controler.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router =Router()
@@ -25,6 +35,14 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJwt, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt,ChangeCurrentPasssword)
+router.route("/current-user").get(verifyJwt,getCurentUser)
+router.route("/update-account").patch(verifyJwt,updateAccountDetails)
+router.route("/avatar").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt,upload.single("/coverImage"),updateCoverImage)
+router.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/history").get(verifyJwt,getWatchHistory)
+
 
 
 export default router
